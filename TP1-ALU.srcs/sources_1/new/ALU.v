ca56feb78db1ba6a838dcb18c3820db4dfccbbcb
Stefano <stefano.itr.unc@gmail.com>
2023-09-13 17:41:44 -0300

    Alu modificado, top level relizado con el clock wizzard comentado. falta terminar las constrains

--- TP1-ALU.srcs/sources_1/new/ALU.v
+++ TP1-ALU.srcs/sources_1/new/ALU.v
@@ -21,17 +21,17 @@
 module ALU
 //parametros 
 #(
-  parameter p_operatorsInputSize =8//32
+  parameter p_dataLength =8//32
 )
 //campo
 (
-  input wire [p_operatorsInputSize-1:0] i_A,
-  input wire [p_operatorsInputSize-1:0] i_B,
+  input wire [p_dataLength-1:0] i_A,
+  input wire [p_dataLength-1:0] i_B,
   input wire [5:0] i_ALUBitsControl,
   
   //debe ser un reg para almacenar y retener su valor entre ciclos de reloj.
-  output reg [p_operatorsInputSize-1:0] o_ALUResult,
-  output wire o_Zero
+  output reg [p_dataLength-1:0] o_ALUResult
+  //output wire o_Zero
   //output wire o_Overflow
 );
 
@@ -43,15 +43,15 @@ module ALU
       6'b100100: o_ALUResult <= i_A & i_B;   // AND 
       6'b100101: o_ALUResult <= i_A | i_B;   // OR
       6'b100110: o_ALUResult <= i_A ^ i_B;   // XOR
-      6'b000011: o_ALUResult <= (i_A >>> (i_B[4:0]));   // SRA (Shift Right Arithmetic)
-      6'b000010: o_ALUResult <= (i_A >> (i_B[4:0]));    // SRL (Shift Right Logical)
+      6'b000011: o_ALUResult <= (i_A >>> i_B);   // SRA (Shift Right Arithmetic)
+      6'b000010: o_ALUResult <= (i_A >> i_B);    // SRL (Shift Right Logical)
       6'b100111: o_ALUResult <= ~(i_A | i_B);   // NOR
       //JAMAS OLVIDARES EL DEFAULT!!!!! si uno se lo olvida, el sintetizador hace cualquier cosa.. crea compuertas flipflops etc.. No queremos un comportamiento anómalo
-      default: o_ALUResult <= {p_operatorsInputSize{'b0}};
+      default: o_ALUResult <= {p_dataLength{1'b0}};
     endcase
   end
 
-  assign o_Zero = (o_ALUResult == {p_operatorsInputSize{'b0}})?1'b1:1'b0;
+  //assign o_Zero = (o_ALUResult == {p_operatorsInputSize{1'b0}})?1'b1:1'b0;
   //assign Overflow = (i_A[31] & i_B[31] & ~o_ALUResult[31]) | (~i_A[31] & ~i_B[31] & o_ALUResult[31]);
 endmodule
 
---
+++ TP1-ALU.srcs/sources_1/new/topLevelAlu.v
@@ -0,0 +1,88 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/10/2023 11:23:15 AM
+// Design Name: 
+// Module Name: topLevelAlu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module topLevelAlu(
+    //inputs
+    input wire switch,
+    input wire button1,//para A
+    input wire button2,//para B
+    input wire button3,//para OPCODE
+    input wire clockCustom,
+    
+    output wire LED,
+    
+    //para el clock
+    input wire resetGral
+    );
+    
+    parameter dataLength=8;
+    parameter opLength=6;
+    
+    reg signed [dataLength-1 : 0] dataA = 0;
+	reg signed [dataLength-1 : 0] dataB = 0;
+	reg [opLength-1 : 0] OPCODE = 0;
+	
+	/*wire o_clockWizzard;
+	wire o_locked;
+	clk_wiz_0
+	u_clk(
+        // Clock out ports  
+        .clk_out1(o_clockWizzard),
+        // Status and control signals               
+        .reset(resetGral), 
+        .locked(o_locked),
+        // Clock in ports
+        .clk_in1(clockCustom)
+	);*/
+	
+	
+	
+	ALU #(.p_dataLength(dataLength))
+	u_alu(
+		.i_A(dataA),
+		.i_B(dataB),
+		.i_ALUBitsControl(OPCODE),
+		.o_ALUResult(LED)
+		);
+		
+	
+	
+    //always @(posedge o_clockWizzard)
+    always @(posedge clockCustom)
+	begin
+        if (button1 == 1'b1) begin//&& o_locked saco esto de todos los ifs else ifs
+            dataA = switch;			
+        end
+        else if (button2 == 1'b1) begin
+            dataB = switch;			
+        end
+        else if (button3 == 1'b1) begin
+            OPCODE = switch;
+        end
+        else begin
+            dataA = dataA;	
+            dataB = dataB;
+            OPCODE = OPCODE;
+        end
+	end
+    
+endmodule
