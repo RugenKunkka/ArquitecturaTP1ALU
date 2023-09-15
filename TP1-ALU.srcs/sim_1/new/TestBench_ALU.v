f9e6c81f7dc8dcff3c9fee04f8c724ab2be9c9be
frankscitech <82125328+frankscitech@users.noreply.github.com>
2023-09-15 20:38:45 -0300

    Entradas aleatorias

--- TP1-ALU.srcs/sim_1/new/TestBench_ALU.v
+++ TP1-ALU.srcs/sim_1/new/TestBench_ALU.v
@@ -23,59 +23,81 @@
 `define dataLength 4
 `define opLength 4
 
-module TestBench_ALU;
+module TestBench_ALU();
 
- parameter dataLength=`dataLength;
-  parameter opLength=`opLength;
-//todos los input wire pasan a reg
-  reg [dataLength-1:0] i_A;
-  reg [dataLength-1:0] i_B;
-  reg [opLength-1:0] i_ALUBitsControl;
-  
-//todos los output pasan a wire
-  wire [dataLength-1:0] o_ALUResult;
+    parameter dataLength=`dataLength;
+    parameter opLength=`opLength;
+    
+    //todos los input wire pasan a reg
+    reg [dataLength-1:0] _i_A;
+    reg [dataLength-1:0] _i_B;
+    reg [opLength-1:0] _i_ALUBitsControl;
+    
+    //todos los output pasan a wire
+    wire [dataLength-1:0] _o_ALUResult;
+    wire  _o_Zero;
 
-//instancio el módulo de la ALU
-//uut= unit under test 
-ALU #(
-    .p_operatorsInputSize(8)
-  ) 
-  uut_ALU (
-    .i_A(i_A),
-    .i_B(i_B),
-    .i_ALUBitsControl(i_ALUBitsControl),
-    .o_ALUResult(o_ALUResult)
-  );
+    //instancio el m?dulo de la ALU
+    //uut= unit under test 
+    ALU #(
+        .p_operatorsInputSize(4)
+    ) 
+    uut_ALU (
+        .i_A(_i_A),
+        .i_B(_i_B),
+        .i_ALUBitsControl(_i_ALUBitsControl),
+        .o_ALUResult(_o_ALUResult),
+        .o_Zero(_o_Zero)
+    );
 
-/*localparam ADD = 6'b100000;
-localparam SUB = 6'b100010;
-localparam AND = 6'b100100;
-localparam OR  = 6'b100101;
-localparam XOR = 6'b100110;
-localparam SRA = 6'b000011;
-localparam SRL = 6'b000010;
-localparam NOR = 6'b100111;*/
-
-localparam ADD = 4'b0001;
-localparam SUB = 4'b0010;
-localparam AND = 4'b0011;
-localparam OR  = 4'b0101;
-localparam XOR = 4'b0111;
-localparam SRA = 4'b1000;
-localparam SRL = 4'b1100;
-localparam NOR = 4'b1110;
+    /*localparam ADD = 6'b100000;
+    localparam SUB = 6'b100010;
+    localparam AND = 6'b100100;
+    localparam OR  = 6'b100101;
+    localparam XOR = 6'b100110;
+    localparam SRA = 6'b000011;
+    localparam SRL = 6'b000010;
+    localparam NOR = 6'b100111;*/
+    
+    localparam ADD = 4'b0001;
+    localparam SUB = 4'b0010;
+    localparam AND = 4'b0011;
+    localparam OR  = 4'b0101;
+    localparam XOR = 4'b0111;
+    localparam SRA = 4'b1000;
+    localparam SRL = 4'b1100;
+    localparam NOR = 4'b1110;
 
   
 initial
 begin
-    i_A = 4'd6;//decimal de 8 bits
-    i_B = 4'd2;
-    i_ALUBitsControl = ADD;//suma
-    
-    #500
-    
-    i_ALUBitsControl = SUB;
-
+    #0
+    _i_A = {dataLength{1'b0}};
+    _i_B = {dataLength{1'b0}};
+    #50
+    _i_ALUBitsControl = ADD;
+    #50
+    _i_ALUBitsControl = SUB;
+    #50
+    _i_ALUBitsControl = AND;
+    #50
+    _i_ALUBitsControl = OR;
+    #50
+    _i_ALUBitsControl = XOR;
+    #50
+    _i_ALUBitsControl = SRA;
+    #50
+    _i_ALUBitsControl = SRL;
+    #50
+    _i_ALUBitsControl = NOR;
+       
 end
+
+// Genera numeros aleatorios cada 60 steps del simulador
+always begin
+  #60
+    _i_A = $random;
+    _i_B = $random;
+  end
 
 endmodule
